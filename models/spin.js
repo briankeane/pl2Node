e22539bfb2306957069548b7ec2da6305e1499a1
briankeane <lonesomewhistle@gmail.com>
2015-02-02 09:07:17 -0600

    added endtime and duration virtuals to spin

--- models/spin.js
+++ models/spin.js
@@ -1,14 +1,41 @@
 var db = require('../db');
 var Station = require('./station');
+// var moment = require('moment');
+// moment().format();
 var audioBlockSchema = require('./audioBlockSchema');
 var AudioBlock = db.model('AudioBlock', audioBlockSchema);
 var timestamps = require('mongoose-timestamp');
 
 var spinSchema = db.Schema({
-  airtime:            { type: Date },
   playlistPosition:   { type: Number },
   _audioBlock:        { type: db.Schema.ObjectId, ref: 'AudioBlock' },
-  _station:           { type: db.Schema.ObjectId, ref: 'Station' }
+  _station:           { type: db.Schema.ObjectId, ref: 'Station' },
+  airtime:            { type: Date },
+  durationOffset:     { type: Number, default: 0 }
+}, {
+  toObject: { getters: true }
+});
+
+spinSchema.virtual('endTime').get(function () {
+  // if it's missing the audioBlock, duration, or airtime, return null
+  if (!((this.airtime) && (this._audioBlock) && (this._audioBlock.duration))) {
+    return null;
+  } else {
+    return new Date(this.airtime.getTime() + this.duration);
+  }
+});
+
+spinSchema.virtual('duration').get(function () {
+  // if something is stored in _audioBlock
+  if (this._audioBlock) {
+
+    // get _audioBlock if it hasn't already been populated
+    if (this._audioBlock.duration) {
+      return this._audioBlock.duration + this.durationOffset;
+    } else {
+      return null;                    // Is this proper treatment?
+    } 
+  }
 });
 
 spinSchema.plugin(timestamps);
--- test/server/models/spin.spec.js
+++ test/server/models/spin.spec.js
@@ -33,15 +33,18 @@ describe('a spin', function (done) {
           spin1 = new Spin({ _station: station.id,
                              playlistPosition: 2,
                              _audioBlock: song.id,
-                             airtime: new Date(2014,1,1,12) });
+                             airtime: new Date(2014,1,1,12) 
+                           });
           spin2 = new Spin({ _station: station.id,
                              playlistPosition: 3,
                              _audioBlock: commentary.id,
-                             airtime: new Date(2014,1,1,12) });
+                             airtime: (new Date(2014,1,1,12)) 
+                           });
           spin3 = new Spin({ _station: station.id,
                              playlistPosition: 4,
                              _audioBlock: song.id,
-                             airtime: new Date(2014,1,1,12) });
+                             airtime: (new Date(2014,1,1,12)) 
+                           });
 
           specHelper.saveAll([spin1, spin2, spin3], function (err, models) {
             done();
@@ -68,7 +71,29 @@ describe('a spin', function (done) {
     });
   });
 
-  it("caluclates it's endTime", function (done) {
-    expect(spin1.endTime.getTime()).to.equal(20)
-  })
+  it("calculates it's duration & returns null if not populated", function (done) {
+    expect(spin1.duration).to.equal(null);  // null before population
+    
+    spin1.populate('_audioBlock', function (err, spin1Populated) {
+      expect(spin1Populated.duration).to.equal(180000);
+      
+      // check for change wiith durationOffset change
+      spin1.durationOffset = 10;
+      expect(spin1Populated.duration).to.equal(180010);
+      done();
+    });
+  });
+
+  it("calculates it's endtime & returns null if not populated", function (done) {
+    expect(spin1.endTime).to.equal(null);  // null before population
+    
+    spin1.populate('_audioBlock', function (err, spin1Populated) {
+      expect(spin1Populated.endTime.getTime()).to.equal(new Date(2014,1,1,12,3).getTime());
+      
+      // check for change wiith durationOffset change
+      spin1.durationOffset = 10;
+      expect(spin1Populated.endTime.getTime()).to.equal(new Date(2014,1,1,12,3,0,10).getTime());
+      done();
+    });
+  });
 });
